43c84dae6178da8a639bba78939e80b87d80d505
Milos Ceculovic <mceculovic2819rn@raf.rs>
2022-02-10 02:38:59 +0100

    committing

--- routes/movies.js
+++ routes/movies.js
@@ -26,7 +26,7 @@ route.get('/movies', async (req, res) => {
     let movies = null;
     try{
         movies = await Movie.findAll({
-            include: [Actor,Director, {model: Genre, as:'genre'}]
+            include: [Actor,Director, 'genre']
         });
         res.json(movies);
     }catch(error){
